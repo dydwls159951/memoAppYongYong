9f8bc00fed6268504e0d4e0c9e68c70789901645
Yongjin Kwon <dydwls159951@gmail.com>
2021-01-04 22:04:01 +0900

    Initial Commit

---
+++ memoAppYongYong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memoAppYongYong
+//
+//  Created by 권용진 on 2021/01/04.
+//  Copyright © 2021 권용진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
